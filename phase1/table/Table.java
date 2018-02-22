85b06f7844773ece4def718f1a6dad6e28188a4f
Yujie <emolangshen@gmail.com>
2018-02-21 20:42:38 -0500

    Table variable and method created.

--- phase1/table/Table.java
+++ phase1/table/Table.java
@@ -1,4 +1,20 @@
 package table;
 
 public class Table {
+    int tableNum;
+    Server server;
+    Order order;
+    public Table(int  tableNum, Server server){
+        this.tableNum = tableNum;
+        this.server = server;
+        order = new Order(this);
+    }
+    public void order(Dish dish){
+        server.order(this, dish);
+    }
+    public String bill(){
+        String bill = ""+tableNum;
+        return bill + order.toString();
+    }
+    
 }
