b857f49112066126b1035fd3dae543f5924f8aae
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-02-23 18:46:24 -0500

    Implementing structure of 'employee' package (in progress)

--- phase1/employee/EmployeeManager.java
+++ phase1/employee/EmployeeManager.java
@@ -1,4 +1,7 @@
 package employee;
 
 public class EmployeeManager {
+  private Cook cook;
+  private Server server;
+  private Receiver receiver;
 }
---
+++ phase1/employee/Server.java
@@ -0,0 +1,14 @@
+package employee;
+
+import java.util.ArrayList;
+
+public class Server {
+  private ArrayList listOfOrders;
+  private Cook cook;
+
+  void setCook(Cook c) {
+    cook = c;
+  }
+
+  public void sendOrderToCook() {}
+}
