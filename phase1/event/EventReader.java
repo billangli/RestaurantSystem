1090e6201f94ca506b968ca629c5b24640a41ae2
Bill-linux <liang20>
2018-02-25 17:02:04 -0500

    Created and defined methods in EventReader.java and created the file event.txt in event package.

---
+++ phase1/event/EventReader.java
@@ -0,0 +1,24 @@
+/*
+  EventReader.java
+  This is a helper class that reads the event.txt file
+
+  Created by Bill Ang Li
+  Feb. 25th, 2018
+ */
+
+package event;
+
+import java.util.Queue;
+
+public class EventReader {
+    public static final String file = "event/event.txt";
+
+    /**
+     * Reads the event.txt file and creates events and adds them to the queue
+     *
+     * @param eventQueue is the queue that the events will be added to
+     */
+    void readFile(Queue<Event> eventQueue) {
+
+    }
+}
