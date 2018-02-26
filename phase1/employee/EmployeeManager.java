d4bf9c8e3d32066f2c9bbb2cc80d849c2be8afe3
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-02-25 21:06:47 -0500

    Add some variables in Receiver, EmployeeManager class.

--- phase1/employee/EmployeeManager.java
+++ phase1/employee/EmployeeManager.java
@@ -1,7 +1,27 @@
 package employee;
 
+import java.util.ArrayList;
+
 public class EmployeeManager {
-  private Cook cook;
-  private Server server;
-  private Receiver receiver;
+  private ArrayList<Cook> cookList;
+  private ArrayList<Server> serverList;
+  private ArrayList<Receiver> receiverList;
+
+  public EmployeeManager() {
+    cookList = new ArrayList<>();
+    serverList = new ArrayList<>();
+    receiverList = new ArrayList<>();
+  }
+
+  public void addCook(String name, int id) {
+    cookList.add(new Cook(name, id));
+  }
+
+  public void addServer(String name, int id) {
+    serverList.add(new Server(name, id));
+  }
+
+  public void addReceiver(String name, int id) {
+    receiverList.add(new Receiver(name, id));
+  }
 }
