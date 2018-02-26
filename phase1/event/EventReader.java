fdd357c976a5a910558e1716183601f796928c4b
Ang Li <billbillbill.li@mail.utoronto.ca>
2018-02-26 12:55:43 -0500

    Defined addLinesToQueue() in EventReader.java and parseEvent() in Event.java.

--- phase1/event/EventReader.java
+++ phase1/event/EventReader.java
@@ -8,17 +8,36 @@
 
 package event;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class EventReader {
-    public static final String file = "event/event.txt";
+    private static final String file = "event/event.txt";
+    private ArrayList<String> lines = new ArrayList<>();
 
     /**
      * Reads the event.txt file and creates events and adds them to the queue
      *
      * @param eventQueue is the queue that the events will be added to
      */
-    void readFile(Queue<Event> eventQueue) {
+    void readFile(Queue<Event> eventQueue) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+
+        addLinesToQueue(lines, eventQueue);
+    }
+
+    /**
+     * Creates Events from info in lines and adds the events to the eventQueue
+     *
+     * @param lines is a list of lines (read from event.txt) that represent events
+     * @param eventQueue is the queue of events
+     */
+    private void addLinesToQueue(ArrayList<String> lines, Queue<Event> eventQueue) {
 
     }
 }
