8ea9babfd8f3c6630fa047642f21ade8d10121f5
Donghee Kim <rammangst@gmail.com>
2018-02-26 12:37:11 -0500

    Added InventoryManager, Inventory and Menu class
    Also modified questions

---
+++ phase1/inventory/Inventory.java
@@ -0,0 +1,21 @@
+package inventory;
+
+import java.util.HashMap;
+
+public class Inventory {
+    HashMap<String, Integer> IngredientsInventory = new HashMap<>();
+    HashMap<String, Integer> ThresholdInventory = new HashMap<>();
+
+    // read off the txtfile
+    public Inventory() {
+
+    }
+
+    // read off the menu and just set arbitrary value for each ingredient
+    public Inventory(Menu menu) {
+
+    }
+
+
+
+}
---
+++ phase1/inventory/Menu.java
@@ -0,0 +1,13 @@
+package inventory;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<Dish> Dishes = new ArrayList<>();
+
+
+
+
+
+}
