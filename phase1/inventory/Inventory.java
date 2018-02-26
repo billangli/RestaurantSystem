c10aec37465ed36e759b68b94561d17c6928b92a
Donghee Kim <rammangst@gmail.com>
2018-02-26 12:51:00 -0500

    - Implemented Jerry's implementation of Dish class  - Implemented the constructor for the Inventory class

--- phase1/inventory/Inventory.java
+++ phase1/inventory/Inventory.java
@@ -1,20 +1,31 @@
 package inventory;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Inventory {
-    HashMap<String, Integer> IngredientsInventory = new HashMap<>();
-    HashMap<String, Integer> ThresholdInventory = new HashMap<>();
+    private static HashMap<String, Integer> IngredientsInventory = new HashMap<>();
+    private static HashMap<String, Integer> ThresholdInventory = new HashMap<>();
 
-    // read off the txtfile
-    public Inventory() {
 
-    }
 
     // read off the menu and just set arbitrary value for each ingredient
     public Inventory(Menu menu) {
+        for (Dish dish : menu.Dishes) {
+            IngredientsInventory.put(dish.getName(), 10000);
+
+        }
 
     }
+
+    public void alertLowIngredient() {
+        Iterator it = IngredientsInventory.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.entry())
+        }
+    }
+
 
 
 
