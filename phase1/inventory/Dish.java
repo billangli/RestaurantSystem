6797bd88637850977560bd4809498f218a4846af
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-03-04 16:32:51 -0500

    Dish.java: google-java-format and implemented updateIngredientsStock() method.

--- phase1/inventory/Dish.java
+++ phase1/inventory/Dish.java
@@ -5,56 +5,52 @@ import java.util.HashMap;
 import table.Table;
 
 public class Dish {
-    private String name;
-    private HashMap<String, Ingredient> ingredientsRequired = new HashMap<String, Ingredient>();
+  private String name;
+  private HashMap<String, Ingredient> ingredientsRequired = new HashMap<>();
 
   private int cost;
   //    private int dishNumber;
   private boolean isReady;
   Table table;
 
-    public Dish(String name, int price, String[] ingredients){
-        this.name = name;
-        this.cost = price;
-        for(String ingredient: ingredients){
-            String[] item = ingredient.split(":");
-            int[] limit = {Integer.parseInt(item[2]),Integer.parseInt(item[3])};
-            Ingredient in = new Ingredient(item[0],Integer.parseInt(item[1]),limit);
-            ingredientsRequired.put(item[0],in);
-        }
+  public Dish(String name, int price, String[] ingredients) {
+    this.name = name;
+    this.cost = price;
+    for (String ingredient : ingredients) {
+      String[] item = ingredient.split(":");
+      int[] limit = {Integer.parseInt(item[2]), Integer.parseInt(item[3])};
+      Ingredient in = new Ingredient(item[0], Integer.parseInt(item[1]), limit);
+      ingredientsRequired.put(item[0], in);
+    }
 
     isReady = false;
   }
 
-  public Dish(Dish d){
-        this.name = d.getName();
-        this.cost = d.getCost();
-        this.ingredientsRequired = d.ingredientsRequired;
-        isReady = false;
+  public Dish(Dish d) {
+    this.name = d.getName();
+    this.cost = d.getCost();
+    this.ingredientsRequired = d.ingredientsRequired;
+    isReady = false;
   }
 
-    // NEED MODIFICATION
-    public void adjustIngredient(String ingredientName, int amount){
-        if(ingredientsRequired.get(ingredientName).allowed(amount, Inventory.getIngredient(ingredientName))){
-            ingredientsRequired.get(ingredientName).adjust(amount);
-        }
+  // NEED MODIFICATION
+  public void adjustIngredient(String ingredientName, int amount) {
+    if (ingredientsRequired
+        .get(ingredientName)
+        .allowed(amount, Inventory.getIngredient(ingredientName))) {
+      ingredientsRequired.get(ingredientName).adjust(amount);
     }
+  }
 
-    public void updateIngredientsStock(int amount) {
-        for (String ingredientName : ingredientsRequired.keySet()) {
-            Inventory.modifyIngredientQuantity(ingredientName, amount);
-        }
-
-
-
-
-//        for (Map.Entry me : ingredientsRequired.entrySet()) {
-//            Ingredient item = (Ingredient)(me.getValue());
-//            String name = (String)(me.getKey());
-//            inventory.getIngredient(name).adjust(item.getQuantity());
-//        }
-
+    /**
+     * Subtracts all the amounts of ingredients used in inventory to make this dish.
+     */
+  public void updateIngredientsStock() {
+    for (String ingredName : ingredientsRequired.keySet()) {
+      Inventory.modifyIngredientQuantity(
+          ingredName, -1 * ingredientsRequired.get(ingredName).getQuantity());
     }
+  }
 
   public void setTable(Table t) {
     table = t;
@@ -92,7 +88,7 @@ public class Dish {
     table.addCost(this);
   }
 
-  public void isCancelled(){
+  public void isCancelled() {
     cost = 0;
   }
 }
