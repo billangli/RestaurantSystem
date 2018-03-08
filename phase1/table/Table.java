32e77db65216e15de0edf3ba709f46ed85248bca
Donghee Kim <rammangst@gmail.com>
2018-03-08 14:34:29 -0500

    spelling errors fixed

--- phase1/table/Table.java
+++ phase1/table/Table.java
@@ -5,10 +5,7 @@ import inventory.Dish;
 
 import java.util.ArrayList;
 
-/**
- * represent each table in the restaurant.
- * store the customer's orders and number number
- */
+/** represent each table in the restaurant. store the customer's orders and number number */
 public class Table {
   private int tableNum;
   private float cost;
@@ -16,23 +13,18 @@ public class Table {
   private ArrayList<Order> order;
 
   /**
-   * initialize the table with its number
-   * and cost of 0
+   * initialize the table with its number and cost of 0
+   *
    * @param tableNum the table number
    */
-
   public Table(int tableNum) {
     this.tableNum = tableNum;
     order = new ArrayList<Order>();
     cost = 0;
   }
 
-
-  /**
-   * clear the table for new customer
-   * no server and empty order
-   */
-  public void clear(){
+  /** clear the table for new customer no server and empty order */
+  public void clear() {
     server = null;
     cost = 0;
     order = new ArrayList<>();
@@ -41,6 +33,7 @@ public class Table {
 
   /**
    * this server serves this table
+   *
    * @param server the server that serving this table
    */
   public void serve(Server server) {
@@ -49,16 +42,16 @@ public class Table {
 
   /**
    * add the dish price into table's price
+   *
    * @param d a dish object
    */
   public void addCost(Dish d) {
     cost += d.getCost();
   }
 
-
   /**
-   * add this order to this table
-   * also add table to this order
+   * add this order to this table also add table to this order
+   *
    * @param newOrder the order that made by the customer
    */
   public void addOrder(Order newOrder) {
@@ -67,7 +60,8 @@ public class Table {
   }
 
   /**
-   *return the table number
+   * return the table number
+   *
    * @return table number
    */
   public int getTableNum() {
@@ -75,18 +69,17 @@ public class Table {
   }
 
   /**
-   *return an arraylist of orders
-   * @return orders in arraylist
+   * return an ArrayList of orders
+   *
+   * @return orders in ArrayList
    */
   public ArrayList<Order> getOrder() {
     return order;
   }
 
-
   /**
-   * print bill for this table in format of Table number
-   * and all dishes with its price
-   * and the total price
+   * print bill for this table in format of Table number and all dishes with its price and the total
+   * price
    */
   public void printBill() {
     System.out.println("===== <BILL> =====");
@@ -101,6 +94,7 @@ public class Table {
 
   /**
    * return the server id if the server is not null
+   *
    * @return the server id, -1 if no server
    */
   public int getServerId() {
