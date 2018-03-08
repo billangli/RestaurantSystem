2ccc0d926694446955fe322126ab0e61683308d1
Donghee Kim <rammangst@gmail.com>
2018-03-08 15:16:19 -0500

    Improved javadoc and converted to google format

--- phase1/inventory/InventoryIngredient.java
+++ phase1/inventory/InventoryIngredient.java
@@ -1,34 +1,31 @@
 package inventory;
 
-
 /**
  * InventoryIngredient represents the Ingredient in inventory
  *
- * InventoryIngredient methods include checking whether the ingredient in the
- * restaurant inventory is below the lower threshold
+ * <p>InventoryIngredient methods include checking whether the ingredient in the restaurant
+ * inventory is below the lower threshold
  */
-public class InventoryIngredient extends Ingredient{
-    private int lowerThreshold;
+public class InventoryIngredient extends Ingredient {
+  private int lowerThreshold;
 
-    /**
-     *
-     *
-     * @param name the name of this InventoryIngredient
-     * @param quantity the quantity of this InventoryIngredient in stock
-     * @param lowerThreshold the lowerThreshold of this InventoryIngredient; if the quantity of this Ingredient goes
-     *                       below this lowerThreshold, then a restock order will be made
-     */
-    public InventoryIngredient(String name, int quantity, int lowerThreshold) {
-        super(name, quantity);
-        this.lowerThreshold = lowerThreshold;
-    }
+  /**
+   * @param name the name of this InventoryIngredient
+   * @param quantity the quantity of this InventoryIngredient in stock
+   * @param lowerThreshold the lowerThreshold of this InventoryIngredient; if the quantity of this
+   *     Ingredient goes below this lowerThreshold, then a restock order will be made
+   */
+  public InventoryIngredient(String name, int quantity, int lowerThreshold) {
+    super(name, quantity);
+    this.lowerThreshold = lowerThreshold;
+  }
 
-    /**
-     * Returns true iff the quantity of this InventoryIngredient is below the lower threshold
-     *
-     * @return boolean statement
-     */
-    public boolean isLowStock() {
-        return this.lowerThreshold < this.getQuantity();
-    }
+  /**
+   * returns true iff the quantity of this InventoryIngredient is below the lower threshold
+   *
+   * @return boolean statement
+   */
+  public boolean isLowStock() {
+    return this.lowerThreshold < this.getQuantity();
+  }
 }
