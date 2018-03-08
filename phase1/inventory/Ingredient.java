e41e071761ef781f435ebd2a50891d8b771ef078
Donghee Kim <rammangst@gmail.com>
2018-03-08 15:12:23 -0500

    Javadoc

--- phase1/inventory/Ingredient.java
+++ phase1/inventory/Ingredient.java
@@ -1,28 +1,12 @@
 package inventory;
 
-/**
- * Ingredient class represents ingredients that are in the restaurant inventory, ingredient received
- * by a receiver, and also the ingredients required for each Dish. Different ingredient objects will
- * be used to represent inventory ingredient and dish ingredient, since the latter will have a lower
- * and upper threshold that limit the level of adjustment allowed for customization by the
- * customers, represented by the thresholdQuantity; thresholdQuantity[0] represents the lower
- * threshold and the thresholdQuantity[1]. The inventory ingredient will also have lower and upper
- * threshold, the lower threshold representing the quantity of ingredient below which will require a
- * restock order be made for the said ingredient. The upper threshold for the inventory ingredient
- * is the recommended maximum quantity for the ingredient set by the restaurant according to the
- * projected amount used for the given time.
- *
- * <p>Ingredient methods include, but are not limited to, indicating whether the ingredient in the
- * inventory is below the threshold and requires a restock order, indicating whether the given
- * quantity requested by a customer to modify the Dish is acceptable as per the limit set for this
- * ingredient used for this Dish.
- */
+/** Ingredient class represents ingredients. */
 public abstract class Ingredient {
   private String name;
   private int quantity;
 
   /**
-   * Constructor for creating Ingredient that was received by the receiver from reStock order
+   * Constructor for creating Ingredient with name and quantity of the ingredient
    *
    * @param name the name of this Ingredient
    * @param quantity the quantity of this Ingredient
@@ -33,7 +17,7 @@ public abstract class Ingredient {
   }
 
   /**
-   * Return the quantity of this Ingredient.
+   * returns the quantity of this Ingredient.
    *
    * @return the quantity of this Ingredient.
    */
@@ -42,7 +26,7 @@ public abstract class Ingredient {
   }
 
   /**
-   * Set the quantity of this Ingredient.
+   * sets the quantity of this Ingredient.
    *
    * @param quantityUnit the quantity that must be set for this Ingredient
    */
@@ -51,7 +35,7 @@ public abstract class Ingredient {
   }
 
   /**
-   * Return the name of this Ingredient
+   * returns the name of this Ingredient
    *
    * @return the name of this Ingredient
    */
