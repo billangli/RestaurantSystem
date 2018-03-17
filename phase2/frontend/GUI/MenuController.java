793db4d736115656c460f6df58ec7e4cb1ac554d
Yujie <emolangshen@gmail.com>
2018-03-17 15:03:06 -0400

    able to order item on GUI

--- phase2/frontend/GUI/MenuController.java
+++ phase2/frontend/GUI/MenuController.java
@@ -2,6 +2,8 @@ package frontend.GUI;
 
 import backend.inventory.Dish;
 import backend.inventory.Menu;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -14,7 +16,7 @@ import java.util.ResourceBundle;
 public class MenuController  implements Initializable {
     @FXML
     GridPane tableView = new GridPane();
-
+    int numoforder = 0;
     @Override
     public void initialize(URL url, ResourceBundle rb){
         HashMap<String, Dish> dishes = Menu.getDishes(); //TODO should get menu from web Server
@@ -22,11 +24,23 @@ public class MenuController  implements Initializable {
         for(String di: dishes.keySet()){
             Button item = new Button(di);
             item.setId(di);
+            item.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                   Button ordered = new Button(di);
+                   item.setId(""+numoforder);
+                   numoforder++;
+                   tableView.add(ordered,2,numoforder);
+                }
+            });
             tableView.add(item,0,i);
             i++;
 
 
         }
+        Button submit = new Button("order");
+        tableView.add(submit,1,5);
 
     }
+
+
 }
