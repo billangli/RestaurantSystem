3b02b3bb2fa2c742cc7756dfeedc4f1cc3c63833
Bill-linux <liang20>
2018-03-20 20:15:29 -0400

    Added ClientThread

--- phase2/backend/server/Server.java
+++ phase2/backend/server/Server.java
@@ -1,4 +1,24 @@
 package backend.server;
 
-public class Server {
+import javafx.beans.InvalidationListener;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Server implements Observer {
+  private Socket socket;
+  private ServerSocket serverSocket;
+  private ArrayList<Object> clients;
+  private ClientThread clientThread;
+
+  private int port;
+  private boolean isListening;
+
+  @Override
+  public void update(Observable o, Object arg) {
+
+  }
 }
