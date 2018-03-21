1487a0854c4de2a6500d680945eb9fad8654c735
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-03-21 15:09:05 -0400

    Dis-enabled root logger's default console handler.

--- phase2/backend/logger/RestaurantLogger.java
+++ phase2/backend/logger/RestaurantLogger.java
@@ -10,6 +10,9 @@ public class RestaurantLogger {
   private static FileHandler fileHandler = null;
 
   public static void init() {
+    // Disables default root logger's console handler.
+    logger.setUseParentHandlers(false);
+
     try {
       fileHandler = new FileHandler("logFile.log", true);
     } catch (IOException e) {
@@ -24,27 +27,28 @@ public class RestaurantLogger {
     logger.addHandler(fileHandler);
     // Note that Log levels INFO and higher will be automatically written to the console.
 
-    PrintStream outPS = null;
-    try {
-      outPS = new PrintStream(new BufferedOutputStream(new FileOutputStream("logFile.log", true)));
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-//    System.setOut(outPS); // redirect System.out
-//    System.setErr(outPS); // redirect System.err
+    //    PrintStream outPS = null;
+    //    try {
+    //      outPS = new PrintStream(new BufferedOutputStream(new FileOutputStream("logFile.log",
+    // true)));
+    //    } catch (FileNotFoundException e) {
+    //      e.printStackTrace();
+    //    }
+    //    System.setOut(outPS); // redirect System.out
+    //    System.setErr(outPS); // redirect System.err
   }
 
-  //TODO: Delete before finalizing.
-//  public static void main(String[] args) {
-//    init();
-//    System.err.println("serr");
-//    backend.logger.log(Level.INFO, "message 1"); // INFO: Default level.
-//    System.out.println("sout");
-//
-//    // CONFIG Level is lower than INFO level, so it doesn't show on log.
-//    backend.logger.log(Level.CONFIG, "message 2");
-//    System.out.println("sout2");
-//    LoggerTest2.thing();
-//    System.err.println("serr2");
-//  }
+  // TODO: Delete before finalizing.
+  //  public static void main(String[] args) {
+  //    init();
+  //    System.err.println("serr");
+  //    backend.logger.log(Level.INFO, "message 1"); // INFO: Default level.
+  //    System.out.println("sout");
+  //
+  //    // CONFIG Level is lower than INFO level, so it doesn't show on log.
+  //    backend.logger.log(Level.CONFIG, "message 2");
+  //    System.out.println("sout2");
+  //    LoggerTest2.thing();
+  //    System.err.println("serr2");
+  //  }
 }
