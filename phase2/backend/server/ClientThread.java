fd35f11ab1dcf2e345e8cb0fc6b4b8fce632911b
Ang Li <billbillbill.li@mail.utoronto.ca>
2018-03-20 21:30:19 -0400

    Client and Server are now Runnable so they can keep running

--- phase2/backend/server/ClientThread.java
+++ phase2/backend/server/ClientThread.java
@@ -1,11 +1,42 @@
 package backend.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
-class ClientThread {
+class ClientThread implements Runnable {
   private Socket socket;
+  private boolean isRunning;
+  private BufferedReader input;
 
-  ClientThread(Socket socketBuff) {
+  ClientThread(Socket socket) throws IOException {
     this.socket = socket;
+    this.isRunning = true;
+    this.input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+
+    this.run();
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Running this client thread");
+    while (this.isRunning) {
+      try {
+        if (this.input.ready()) {
+          System.out.println(this.input.readLine());
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        this.isRunning = false;
+      }
+    }
+
+    System.out.println("This client thread is closing");
+    try {
+      this.socket.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
--- phase2/frontend/client/Client.java
+++ phase2/frontend/client/Client.java
@@ -6,39 +6,61 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+public class Client implements Runnable {
   private String ip;
   private int port;
   private Socket socket;
+  private boolean isConnected = false;
+
   private BufferedReader input;
   private PrintWriter output;
 
-  Client (String ip, int port) {
+  Client(String ip, int port) throws IOException {
     this.ip = ip;
     this.port = port;
-    this.connect();
+    this.isConnected = this.connect();
+    Thread t = new Thread(this);
+    t.start();
   }
 
-  private void connect() {
-    try {
-      System.out.println("Attempting connection");
+  /**
+   * Connect this Client to the Server
+   */
+  private boolean connect() throws IOException {
+    this.socket = new Socket(this.ip, this.port);
+    this.input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+    this.output = new PrintWriter(this.socket.getOutputStream());
 
-      this.socket = new Socket(this.ip, this.port);
-      this.input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-      this.output = new PrintWriter(this.socket.getOutputStream());
+    System.out.println("Connection successful");
+    return true;
+  }
 
-      System.out.println("Connection successful");
-    } catch (IOException e) {
-      e.printStackTrace();
+  void send(String message) {
+    if (this.isConnected) {
+      System.out.println("Sending " + message);
+      this.output.println(message);
+      this.output.flush();
     }
+  }
 
-    while(true) {
-
+  @Override
+  public void run() {
+    while (this.isConnected) {
+      try {
+        if (this.input.ready()) {
+          // Do something with the input from the server
+          System.out.println(this.input.readLine());
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        this.isConnected = false;
+      }
     }
   }
 
   // TODO: Remove this after testing
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Client client = new Client("127.0.0.1", 6000);
+    client.send("Hello from the client");
   }
 }
