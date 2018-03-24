16406327722ba29376075327cd75c1c5e33e08e5
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-03-24 11:49:26 -0400

    GUI_2 (gui testing package).

--- phase2/frontend/GUI_2/ServerStageController.java
+++ phase2/frontend/GUI_2/ServerStageController.java
@@ -2,41 +2,132 @@ package frontend.GUI_2;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.sqrt;
 
 public class ServerStageController {
+  /*final variables*/
+  private final Color COLOR_OCCUPIED = Color.BLUE;
+  private final Color COLOR_EMPTY = Color.WHITE;
+
+  /*FXML variables*/
+  @FXML Button confirmButtonTakeSeat;
+  @FXML Button confirmButtonClearTable;
+  @FXML Button cancelButton;
+  @FXML private ChoiceBox<String> tableNumberBoxTakeSeat;
+  @FXML private ChoiceBox<String> tableNumberBoxClearTable;
+  @FXML private VBox gridParent;
+  @FXML private HBox hBox1;
+  @FXML private HBox hBox2;
+
+  private int numOfTables;
+  private ArrayList<Rectangle> rectangles;
+  /* Methods */
   @FXML
-  private Rectangle rectangle1,
-      rectangle2,
-      rectangle3,
-      rectangle4,
-      rectangle5,
-      rectangle6,
-      rectangle7,
-      rectangle8,
-      rectangle9,
-      rectangle10,
-      rectangle11,
-      rectangle12;
+  private void initialize() {
+    int size = 15;
+    Rectangle rec1 = new Rectangle(size, size);
+    Label label1 = new Label(" : table is occupied");
+    Rectangle rec2 = new Rectangle(size, size);
+    Label label2 = new Label(" : table is empty");
+
+    rec1.setFill(COLOR_OCCUPIED);
+    rec1.setStroke(Color.BLACK);
+    rec2.setFill(COLOR_EMPTY);
+    rec2.setStroke(Color.BLACK);
+
+    hBox1.getChildren().addAll(rec1, label1);
+    hBox2.getChildren().addAll(rec2, label2);
+    hBox1.setPadding(new Insets(10, 0, 0, 10));
+    hBox2.setPadding(new Insets(10, 0, 0, 10));
+
+    GridPane tableGrid = new GridPane();
+    tableGrid.setHgap(10);
+    tableGrid.setVgap(8);
+    tableGrid.setPadding(new Insets(40, 0, 0, 40));
+
+    // TODO: retrieve numOfTable value from backend.
+    // now I will assume that we have certain number of tables.
+    numOfTables = 4;
+    int sideLength = (int) ceil(sqrt(numOfTables));
+
+    for (int i = 0; i < numOfTables; i++) {
+      Rectangle r = new Rectangle(50, 50);
+      r.setFill(COLOR_EMPTY);
+      r.setStroke(Color.BLACK);
+      Label l = new Label(Integer.toString(i + 1));
+      GridPane.setConstraints(r, i % sideLength, i / sideLength);
+      GridPane.setConstraints(l, i % sideLength, i / sideLength);
+      tableGrid.getChildren().addAll(r, l);
+    }
+
+    gridParent.getChildren().add(tableGrid);
+  }
 
   @FXML
   private void takeSeatClicked() {
     Stage window = new Stage();
     window.initModality(Modality.APPLICATION_MODAL);
-    System.out.println(rectangle1);
 
     Parent root = null;
     try {
       root = FXMLLoader.load(getClass().getResource("/frontend/GUI_2/TakeSeatAlertBox.fxml"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    window.setTitle("Welcome!");
+    window.setScene(new Scene(root, 300, 200));
+    window.setMinWidth(300);
+    window.setMinHeight(200);
+    window.showAndWait();
+  }
+
+  @FXML
+  private void confirmButtonClickedTakeSeat() {
+    // TODO 1: change color of table. Data should come from backend.
+    // TODO 2: should I use Thread?
+
+    // close the window
+    ((Stage) confirmButtonTakeSeat.getScene().getWindow()).close();
+  }
+
+  @FXML
+  private void confirmButtonClickedClearTable() {
+    // TODO 1: change color of table. Data should come from backend.
+    // TODO 2: should I use Thread?
+
+    // close the window
+    ((Stage) confirmButtonClearTable.getScene().getWindow()).close();
+  }
+
+  @FXML
+  private void cancelButtonClicked() {
+    // close the window
+    ((Stage) cancelButton.getScene().getWindow()).close();
+  }
+
+  @FXML
+  private void clearTableClicked() {
+    Stage window = new Stage();
+    window.initModality(Modality.APPLICATION_MODAL);
+
+    Parent root = null;
+    try {
+      root = FXMLLoader.load(getClass().getResource("/frontend/GUI_2/ClearTableAlertBox.fxml"));
     } catch (IOException e) {
       e.printStackTrace();
     }
