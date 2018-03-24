a99030022dcdf4101c3f07677ffab2fe03a4667a
Donghee Kim <rammangst@gmail.com>
2018-03-24 15:46:50 -0400

    - Order.addDish() no longer checks whether there is enough ingredient or update the mirrorQuantity of the ingredients inventory

--- phase2/backend/table/Order.java
+++ phase2/backend/table/Order.java
@@ -1,6 +1,5 @@
 package backend.table;
 
-
 import backend.inventory.Dish;
 
 import java.util.ArrayList;
@@ -24,16 +23,8 @@ public class Order {
    *
    * @param d a dish
    */
-
-
   public void addDish(Dish d) {
-    if (d.canDishBeCooked()) {
-      d.updateProjectedIngredientsStock();
-      dishes.add(d);
-      // TODO: Run the method that checks
-    } else {
-      // TODO: RAISE ERROR? MAYBE CONVERT THIS TRY INSTEAD?
-    }
+    dishes.add(d);
   }
 
   /**
