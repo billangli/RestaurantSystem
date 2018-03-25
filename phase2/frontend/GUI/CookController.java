b7b6f062eb9f0996cdee34b5b722962e1159a1f4
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-03-25 15:42:59 -0400

    GUI and controller for cook

--- phase2/frontend/GUI/CookController.java
+++ phase2/frontend/GUI/CookController.java
@@ -1,32 +1,102 @@
 package frontend.GUI;
 
+import backend.inventory.Dish;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 public class CookController {
-    @FXML
-    GridPane tableView = new GridPane();
+  @FXML HBox hBox;
+  @FXML Button getOrderButton;
+  @FXML Label numOfOrderLabel;
+  private int myId;
+  /* table view of dishes in progress */
+  @FXML private TableView<Dish> tableViewDishesInProgress = new TableView<>();
+  @FXML private TableColumn<Dish, String> nameColumn1;
+  @FXML private TableColumn<Dish, Integer> numberColumn1;
 
-    @FXML
-    HBox personal = new HBox();
+  /* table view of dishes to be cooked */
+  @FXML private TableView<Dish> tableViewDishesInQueue = new TableView<>();
+  @FXML private TableColumn<Dish, String> nameColumn2;
+  @FXML private TableColumn<Dish, Integer> numberColumn2;
 
-    private int myId;
-    public void setmyId(int id){
-        myId = id;
-    }
+  public void setmyId(int id) {
+    myId = id;
+  }
 
-    public void initialize(){
-        for(int i = 0; i < 6; i++){
-            Label item = new Label("nothing");
-            item.setId("" + i);
-            tableView.add(item, 0, i);
-        }
-        personal.setSpacing(20);
-        personal.getChildren().add(new Text("nothing"));
-        personal.getChildren().add(new Button("take"));
-    }
+  @FXML
+  private void initialize() {
+//    // Dish Name Column 1
+//    nameColumn1 = new TableColumn<>("Dish name");
+//    nameColumn1.setMinWidth(175);
+//    nameColumn1.setCellValueFactory(new PropertyValueFactory<>("name"));
+//
+//    // Dish Number Column 1
+//    numberColumn1 = new TableColumn<>("Dish number");
+//    numberColumn1.setMinWidth(125);
+//    numberColumn1.setCellValueFactory(new PropertyValueFactory<>("dishNumber"));
+//
+//    // Dish Name Column 2
+//    nameColumn2 = new TableColumn<>("Dish name");
+//    nameColumn2.setMinWidth(175);
+//    nameColumn2.setCellValueFactory(new PropertyValueFactory<>("name"));
+//
+//    // Dish Number Column 2
+//    numberColumn2 = new TableColumn<>("Dish number");
+//    numberColumn2.setMinWidth(125);
+//    numberColumn2.setCellValueFactory(new PropertyValueFactory<>("dishNumber"));
+//
+//    tableViewDishesInProgress = new TableView<>();
+    tableViewDishesInProgress.setItems(getDishesInProgress());
+//    tableViewDishesInProgress.getColumns().addAll(nameColumn1, numberColumn1);
+//
+//    tableViewDishesInQueue = new TableView<>();
+    tableViewDishesInQueue.setItems(getDishesInFirstOrderQueue());
+//    tableViewDishesInQueue.getColumns().addAll(nameColumn2, numberColumn2);
+//
+//    hBox.getChildren().add(tableViewDishesInProgress);
+//    hBox.getChildren().add(tableViewDishesInQueue);
+  }
+
+  private ObservableList<Dish> getDishesInProgress() {
+    ObservableList<Dish> dishes = FXCollections.observableArrayList();
+    // TODO: Retrieve current dishes from backend. Data should be from OrderQueue.DishesInProgress
+    dishes.add(new Dish("Dish bbq", 5, new String[0]));
+
+    return dishes;
+  }
+
+  private ObservableList<Dish> getDishesInFirstOrderQueue() {
+    ObservableList<Dish> dishes = FXCollections.observableArrayList();
+    // TODO 1: Retrieve first order in queue, and get dishes in the first order.
+    // TODO 2: Get number of orders in queue, and set it on label <numOfOrderLabel>
+    dishes.add(new Dish("Dish to be cooked", 6, new String[0]));
+
+    return dishes;
+  }
+
+  public void updateDishesInProgress() {
+    tableViewDishesInProgress.setItems(getDishesInProgress());
+    tableViewDishesInQueue.setItems(getDishesInFirstOrderQueue());
+    tableViewDishesInProgress.getColumns().addAll(nameColumn1, numberColumn1);
+    tableViewDishesInQueue.getColumns().addAll(nameColumn2, numberColumn2);
+  }
+
+  @FXML private void getOrderButtonClicked() {
+      ObservableList<Dish> dishes = tableViewDishesInProgress.getItems();
+      dishes.addAll(tableViewDishesInQueue.getItems());
+      tableViewDishesInProgress.setItems(dishes);
+  }
+
+  @FXML private void finishedButtonClicked() {
+      Dish dish = tableViewDishesInProgress.getSelectionModel().getSelectedItem();
+      //TODO: remove dish in progress queue, and update the tableview
+  }
 }
