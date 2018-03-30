33ae7630678fe7da2076a3a32e30c9b8b466930e
Gihyun Kim <gihyun.kim@mail.utoronto.ca>
2018-03-29 20:44:14 -0400

    take seat cannot have 0 customer as input

--- phase2/frontend/GUI/TakeSeatController.java
+++ phase2/frontend/GUI/TakeSeatController.java
@@ -5,68 +5,69 @@ import frontend.client.Client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
-/**
- * the controller for take seat GUI
- */
+/** the controller for take seat GUI */
 public class TakeSeatController {
-    @FXML private TextField tf;
-    @FXML private Button confirmButton, cancelButton;
-    int tableNumber;
-    private int myId;
-    ArrayList<Rectangle> rectangleArrayList = new ArrayList<>();
+  @FXML private TextField tf;
+  @FXML private Button confirmButton, cancelButton;
+  int tableNumber;
+  private int myId;
+  ArrayList<Rectangle> rectangleArrayList = new ArrayList<>();
 
-    private final Client client = Client.getInstance();
+  private final Client client = Client.getInstance();
 
-    public void setMyId(int id) {
-        this.myId = id;
+  public void setMyId(int id) {
+    this.myId = id;
+  }
+
+  /**
+   * set the table number for the take seat button
+   *
+   * @param tableNumber the table that the customer is taking seat
+   */
+  public void setTableNumber(int tableNumber) {
+    this.tableNumber = tableNumber;
+  }
+
+  @FXML
+  private void confirmButtonClicked() {
+    if (tf.getText().length() > 0 && Integer.parseInt(tf.getText()) > 0) {
+      // Change the color of the table into COLOR_OCCUPIED
+
+      ArrayList<Object> parameters = new ArrayList<>();
+      parameters.add(tableNumber);
+      parameters.add(Integer.parseInt(tf.getText())); // TODO: SHould be number of customers
+      client.sendEvent(
+          Packet.TAKESEAT,
+          parameters); // TODO: In backend, call takeSeat() method (need number of customers)
+      // (serverObj).takeSeat(TableManager.getTable(tableNumber - 1),
+      // Integer.parseInt(tf.getText()));
+      /* ------------------------------------------------------------------------------------------ */
+
+      // close the window
+      ((Stage) confirmButton.getScene().getWindow()).close();
     }
+  }
 
-    /**
-     * set the table number for the take seat button
-     * @param tableNumber the table that the customer is taking seat
-     */
-    public void setTableNumber(int tableNumber){
-        this.tableNumber = tableNumber;
-    }
+  /** cancel the current action */
+  @FXML
+  private void cancelButtonClicked() {
+    // close the window
+    ((Stage) cancelButton.getScene().getWindow()).close();
+  }
 
-    @FXML private void confirmButtonClicked() {
-        if (tf.getText().length()>0) {
-            // Change the color of the table into COLOR_OCCUPIED
-
-            ArrayList<Object> parameters = new ArrayList<>();
-            parameters.add(tableNumber);
-            parameters.add(Integer.parseInt(tf.getText())); // TODO: SHould be number of customers
-            client.sendEvent(Packet.TAKESEAT, parameters);// TODO: In backend, call takeSeat() method (need number of customers)
-            //(serverObj).takeSeat(TableManager.getTable(tableNumber - 1), Integer.parseInt(tf.getText()));
-            /* ------------------------------------------------------------------------------------------ */
-
-            // close the window
-            ((Stage) confirmButton.getScene().getWindow()).close();
-        }
-    }
-
-    /**
-     * cancel the current action
-     */
-    @FXML private void cancelButtonClicked() {
-        // close the window
-        ((Stage) cancelButton.getScene().getWindow()).close();
-    }
-
-    /**
-     * start up the code after init
-     */
-    public void start(){
-        tf.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue.matches("\\d*")) {
+  /** start up the code after init */
+  public void start() {
+    tf.textProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              if (!newValue.matches("\\d*")) {
                 tf.setText(newValue.replaceAll("[^\\d]", ""));
-            }
-        });
-    }
+              }
+            });
+  }
 }
